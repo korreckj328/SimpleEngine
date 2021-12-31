e696200ebdf4a52b3616d67d6bddb3eddd6beca8
Jeremiah Korreck <korreckj328@gmail.com>
2021-12-31 14:45:17 -0500

    Initial Commit

---
+++ SimpleEngine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SimpleEngine
+//
+//  Created by Jeremiah Korreck on 12/31/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
